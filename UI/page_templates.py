7bb92b523c909ac3dd571d0d78d8683698023371
Elliot Stead <elliotmstead@gmail.com>
2025-04-15 08:56:22 -0400

    Update page_templates.py

--- UI/page_templates.py
+++ UI/page_templates.py
@@ -14,8 +14,14 @@ class Templates:
         button2 = Button(button_texts[1], 20, 120, 200, 50, normal_color=self.normal_color, text_color=self.text_color, font=self.font, callback=callbacks[1])
 
         page = Page([button1, button2], title, self.normal_color, self.text_color, font=self.font)
-    
+
         return page
-        
-    def contents(self, title, button_texts, callbacks):
-        pass
+
+    def contents(self, title, pages, switch_page):
+        button1 = Button(pages[0].get_title(), 20, 10, 200, 50, normal_color=self.normal_color, text_color=self.text_color, font=self.font, callback=lambda: switch_page(pages[0]))
+        button2 = Button(pages[1].get_title(), 20, 70, 200, 50, normal_color=self.normal_color, text_color=self.text_color, font=self.font, callback=lambda: switch_page(pages[1]))
+        button3 = Button(pages[2].get_title(), 20, 130, 200, 50, normal_color=self.normal_color, text_color=self.text_color, font=self.font, callback=lambda: switch_page(pages[2]))
+
+        page = Page([button1, button2, button3], title, self.normal_color, self.text_color, font=self.font)
+
+        return page
